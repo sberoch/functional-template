ba580f9c3ba9af71e20873c8d1795d8ede9d460e
sberoch <sberoch@gmail.com>
2020-11-13 23:48:08 -0300

    exercise 3

--- src/exercise3.clj
+++ src/exercise3.clj
@@ -3,7 +3,10 @@
 (defn fibonacci
   "Calculates the fibonacci result fo the given input value."
   [x]
-  (throw (Exception. "Not implemented."))
+  (case x 
+  			0 x 
+  			1 x
+  			(+ (fibonacci (- x 1)) (fibonacci (- x 2))))
 )
 
 
